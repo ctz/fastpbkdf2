4d9640aee6fcb9b6189490dc29e83b268e2799a5
Joseph Birr-Pixton <jpixton@gmail.com>
2015-07-29 11:57:27 +0100

    benchmulti: run for increasing lengths

--- benchmulti.c
+++ benchmulti.c
@@ -33,24 +33,29 @@ static double clock2secs(clock_t start, clock_t end)
   return (end - start) / (double) sysconf(_SC_CLK_TCK);
 }
 
-static void sha1(uint32_t repeat, uint32_t iterations)
+static void sha1(uint32_t repeat, uint32_t iterations, size_t n)
 {
   uint8_t out[64];
 
-  clock_t cpu_end, cpu_start = cpu_now();
-  double wall_end, wall_start = wall_now();
+  assert(sizeof(out) >= n);
+
+  clock_t cpu_end, cpu_start;
+  double wall_end, wall_start;
+  
+  cpu_start = cpu_now();
+  wall_start = wall_now();
 
   for (uint32_t i = 0; i < repeat; i++)
     PKCS5_PBKDF2_HMAC_SHA1(PASSWORD, SALT,
                            iterations,
-                           (int) sizeof(out), out);
+                           (int) n, out);
   wall_end = wall_now();
   cpu_end = cpu_now();
 
   printf("openssl,sha1,%u,%u,%zu,%g,%g\n",
          iterations,
          repeat,
-         sizeof out,
+         n,
          clock2secs(cpu_start, cpu_end),
          wall_end - wall_start);
 
@@ -60,21 +65,24 @@ static void sha1(uint32_t repeat, uint32_t iterations)
   for (uint32_t i = 0; i < repeat; i++)
     fastpbkdf2_hmac_sha1(PASSWORD, SALT,
                          iterations,
-                         out, sizeof out);
+                         out, n);
   wall_end = wall_now();
   cpu_end = cpu_now();
 
   printf("fastpbkdf2,sha1,%u,%u,%zu,%g,%g\n",
          iterations,
          repeat,
-         sizeof out,
+         n,
          clock2secs(cpu_start, cpu_end),
          wall_end - wall_start);
 }
 
 int main(void)
 {
-  sha1(2, 1 << 22);
+  sha1(1, 1 << 22, 16);
+  sha1(1, 1 << 22, 32);
+  sha1(1, 1 << 22, 48);
+  sha1(1, 1 << 22, 64);
 
   return 0;
 }
