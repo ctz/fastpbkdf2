5127ba5f63200fc5a30856bcb9faa7e308000643
Joseph Birr-Pixton <jpixton@gmail.com>
2016-03-14 19:43:45 +0000

    SHA1: calculate W terms as needed

--- sha1.inc.c
+++ sha1.inc.c
@@ -48,98 +48,101 @@ static void sha1_raw_transform(const uint32_t state_in[5],
            d = state_in[3],
            e = state_in[4];
 
-  uint32_t w[80];
+  /* Instead of computing all 80 terms of W, we keep a
+   * 'circular window' of size 16. This is enough to calculate
+   * the next value in the window.  Accesses therefore need to
+   * be reduced mod 16. */
+  uint32_t W[16];
 
-  for (int i = 0; i < 16; i++)
-    w[i] = inp[i];
-  for (int i = 16; i < 80; i++)
-    w[i] = rotl(w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16], 1);
+#define Wi(i) W[i] = inp[i]
+#define Wn(n) W[n & 0xf] = rotl(W[(n - 3) & 0xf] ^ W[(n - 8) & 0xf] ^ W[(n - 14) & 0xf] ^ W[(n - 16) & 0xf], 1)
 
-#define R0(v, w, x, y, z, B) z += ((w & (x ^ y)) ^ y) + B + 0x5a827999 + rotl(v, 5); w = rotl(w, 30)
-#define R1(v, w, x, y, z, B) z += (w ^ x ^ y) + B + 0x6ed9eba1 + rotl(v, 5); w = rotl(w, 30)
-#define R2(v, w, x, y, z, B) z += (((w | x) & y) | (w & x)) + B + 0x8f1bbcdc + rotl(v, 5); w = rotl(w, 30)
-#define R3(v, w, x, y, z, B) z += (w ^ x ^ y) + B + 0xca62c1d6 + rotl(v, 5); w = rotl(w, 30)
+#define R0(v, w, x, y, z, i) z += ((w & (x ^ y)) ^ y) + W[i & 0xf] + 0x5a827999 + rotl(v, 5); w = rotl(w, 30)
+#define R1(v, w, x, y, z, i) z += (w ^ x ^ y) + W[i & 0xf] + 0x6ed9eba1 + rotl(v, 5); w = rotl(w, 30)
+#define R2(v, w, x, y, z, i) z += (((w | x) & y) | (w & x)) + W[i & 0xf] + 0x8f1bbcdc + rotl(v, 5); w = rotl(w, 30)
+#define R3(v, w, x, y, z, i) z += (w ^ x ^ y) + W[i & 0xf] + 0xca62c1d6 + rotl(v, 5); w = rotl(w, 30)
 
-  R0(a, b, c, d, e, w[0]);
-  R0(e, a, b, c, d, w[1]);
-  R0(d, e, a, b, c, w[2]);
-  R0(c, d, e, a, b, w[3]);
-  R0(b, c, d, e, a, w[4]);
-  R0(a, b, c, d, e, w[5]);
-  R0(e, a, b, c, d, w[6]);
-  R0(d, e, a, b, c, w[7]);
-  R0(c, d, e, a, b, w[8]);
-  R0(b, c, d, e, a, w[9]);
-  R0(a, b, c, d, e, w[10]);
-  R0(e, a, b, c, d, w[11]);
-  R0(d, e, a, b, c, w[12]);
-  R0(c, d, e, a, b, w[13]);
-  R0(b, c, d, e, a, w[14]);
-  R0(a, b, c, d, e, w[15]);
-  R0(e, a, b, c, d, w[16]);
-  R0(d, e, a, b, c, w[17]);
-  R0(c, d, e, a, b, w[18]);
-  R0(b, c, d, e, a, w[19]);
-  R1(a, b, c, d, e, w[20]);
-  R1(e, a, b, c, d, w[21]);
-  R1(d, e, a, b, c, w[22]);
-  R1(c, d, e, a, b, w[23]);
-  R1(b, c, d, e, a, w[24]);
-  R1(a, b, c, d, e, w[25]);
-  R1(e, a, b, c, d, w[26]);
-  R1(d, e, a, b, c, w[27]);
-  R1(c, d, e, a, b, w[28]);
-  R1(b, c, d, e, a, w[29]);
-  R1(a, b, c, d, e, w[30]);
-  R1(e, a, b, c, d, w[31]);
-  R1(d, e, a, b, c, w[32]);
-  R1(c, d, e, a, b, w[33]);
-  R1(b, c, d, e, a, w[34]);
-  R1(a, b, c, d, e, w[35]);
-  R1(e, a, b, c, d, w[36]);
-  R1(d, e, a, b, c, w[37]);
-  R1(c, d, e, a, b, w[38]);
-  R1(b, c, d, e, a, w[39]);
-  R2(a, b, c, d, e, w[40]);
-  R2(e, a, b, c, d, w[41]);
-  R2(d, e, a, b, c, w[42]);
-  R2(c, d, e, a, b, w[43]);
-  R2(b, c, d, e, a, w[44]);
-  R2(a, b, c, d, e, w[45]);
-  R2(e, a, b, c, d, w[46]);
-  R2(d, e, a, b, c, w[47]);
-  R2(c, d, e, a, b, w[48]);
-  R2(b, c, d, e, a, w[49]);
-  R2(a, b, c, d, e, w[50]);
-  R2(e, a, b, c, d, w[51]);
-  R2(d, e, a, b, c, w[52]);
-  R2(c, d, e, a, b, w[53]);
-  R2(b, c, d, e, a, w[54]);
-  R2(a, b, c, d, e, w[55]);
-  R2(e, a, b, c, d, w[56]);
-  R2(d, e, a, b, c, w[57]);
-  R2(c, d, e, a, b, w[58]);
-  R2(b, c, d, e, a, w[59]);
-  R3(a, b, c, d, e, w[60]);
-  R3(e, a, b, c, d, w[61]);
-  R3(d, e, a, b, c, w[62]);
-  R3(c, d, e, a, b, w[63]);
-  R3(b, c, d, e, a, w[64]);
-  R3(a, b, c, d, e, w[65]);
-  R3(e, a, b, c, d, w[66]);
-  R3(d, e, a, b, c, w[67]);
-  R3(c, d, e, a, b, w[68]);
-  R3(b, c, d, e, a, w[69]);
-  R3(a, b, c, d, e, w[70]);
-  R3(e, a, b, c, d, w[71]);
-  R3(d, e, a, b, c, w[72]);
-  R3(c, d, e, a, b, w[73]);
-  R3(b, c, d, e, a, w[74]);
-  R3(a, b, c, d, e, w[75]);
-  R3(e, a, b, c, d, w[76]);
-  R3(d, e, a, b, c, w[77]);
-  R3(c, d, e, a, b, w[78]);
-  R3(b, c, d, e, a, w[79]);
+  Wi(0);  R0(a, b, c, d, e, 0);
+  Wi(1);  R0(e, a, b, c, d, 1);
+  Wi(2);  R0(d, e, a, b, c, 2);
+  Wi(3);  R0(c, d, e, a, b, 3);
+  Wi(4);  R0(b, c, d, e, a, 4);
+  Wi(5);  R0(a, b, c, d, e, 5);
+  Wi(6);  R0(e, a, b, c, d, 6);
+  Wi(7);  R0(d, e, a, b, c, 7);
+  Wi(8);  R0(c, d, e, a, b, 8);
+  Wi(9);  R0(b, c, d, e, a, 9);
+  Wi(10); R0(a, b, c, d, e, 10);
+  Wi(11); R0(e, a, b, c, d, 11);
+  Wi(12); R0(d, e, a, b, c, 12);
+  Wi(13); R0(c, d, e, a, b, 13);
+  Wi(14); R0(b, c, d, e, a, 14);
+  Wi(15); R0(a, b, c, d, e, 15);
+  
+  Wn(16); R0(e, a, b, c, d, 16);
+  Wn(17); R0(d, e, a, b, c, 17);
+  Wn(18); R0(c, d, e, a, b, 18);
+  Wn(19); R0(b, c, d, e, a, 19);
+  Wn(20); R1(a, b, c, d, e, 20);
+  Wn(21); R1(e, a, b, c, d, 21);
+  Wn(22); R1(d, e, a, b, c, 22);
+  Wn(23); R1(c, d, e, a, b, 23);
+  Wn(24); R1(b, c, d, e, a, 24);
+  Wn(25); R1(a, b, c, d, e, 25);
+  Wn(26); R1(e, a, b, c, d, 26);
+  Wn(27); R1(d, e, a, b, c, 27);
+  Wn(28); R1(c, d, e, a, b, 28);
+  Wn(29); R1(b, c, d, e, a, 29);
+  Wn(30); R1(a, b, c, d, e, 30);
+  Wn(31); R1(e, a, b, c, d, 31);
+  Wn(32); R1(d, e, a, b, c, 32);
+  Wn(33); R1(c, d, e, a, b, 33);
+  Wn(34); R1(b, c, d, e, a, 34);
+  Wn(35); R1(a, b, c, d, e, 35);
+  Wn(36); R1(e, a, b, c, d, 36);
+  Wn(37); R1(d, e, a, b, c, 37);
+  Wn(38); R1(c, d, e, a, b, 38);
+  Wn(39); R1(b, c, d, e, a, 39);
+  Wn(40); R2(a, b, c, d, e, 40);
+  Wn(41); R2(e, a, b, c, d, 41);
+  Wn(42); R2(d, e, a, b, c, 42);
+  Wn(43); R2(c, d, e, a, b, 43);
+  Wn(44); R2(b, c, d, e, a, 44);
+  Wn(45); R2(a, b, c, d, e, 45);
+  Wn(46); R2(e, a, b, c, d, 46);
+  Wn(47); R2(d, e, a, b, c, 47);
+  Wn(48); R2(c, d, e, a, b, 48);
+  Wn(49); R2(b, c, d, e, a, 49);
+  Wn(50); R2(a, b, c, d, e, 50);
+  Wn(51); R2(e, a, b, c, d, 51);
+  Wn(52); R2(d, e, a, b, c, 52);
+  Wn(53); R2(c, d, e, a, b, 53);
+  Wn(54); R2(b, c, d, e, a, 54);
+  Wn(55); R2(a, b, c, d, e, 55);
+  Wn(56); R2(e, a, b, c, d, 56);
+  Wn(57); R2(d, e, a, b, c, 57);
+  Wn(58); R2(c, d, e, a, b, 58);
+  Wn(59); R2(b, c, d, e, a, 59);
+  Wn(60); R3(a, b, c, d, e, 60);
+  Wn(61); R3(e, a, b, c, d, 61);
+  Wn(62); R3(d, e, a, b, c, 62);
+  Wn(63); R3(c, d, e, a, b, 63);
+  Wn(64); R3(b, c, d, e, a, 64);
+  Wn(65); R3(a, b, c, d, e, 65);
+  Wn(66); R3(e, a, b, c, d, 66);
+  Wn(67); R3(d, e, a, b, c, 67);
+  Wn(68); R3(c, d, e, a, b, 68);
+  Wn(69); R3(b, c, d, e, a, 69);
+  Wn(70); R3(a, b, c, d, e, 70);
+  Wn(71); R3(e, a, b, c, d, 71);
+  Wn(72); R3(d, e, a, b, c, 72);
+  Wn(73); R3(c, d, e, a, b, 73);
+  Wn(74); R3(b, c, d, e, a, 74);
+  Wn(75); R3(a, b, c, d, e, 75);
+  Wn(76); R3(e, a, b, c, d, 76);
+  Wn(77); R3(d, e, a, b, c, 77);
+  Wn(78); R3(c, d, e, a, b, 78);
+  Wn(79); R3(b, c, d, e, a, 79);
 
   state_out[0] = a + state_in[0];
   state_out[1] = b + state_in[1];
@@ -151,6 +154,8 @@ static void sha1_raw_transform(const uint32_t state_in[5],
 #undef R1
 #undef R2
 #undef R3
+#undef Wi
+#undef Wn
 }
 
 static void sha1_convert_input(sha1_block inp32, const uint8_t inp[64])
