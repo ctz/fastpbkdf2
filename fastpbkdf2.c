bb9c8af4fe8adbfa6a1a770d369c6653c65f9240
Joseph Birr-Pixton <jpixton@gmail.com>
2017-10-29 16:55:47 +0000

    Use endian.h rather than GCC builtins
    Patch by mail from Yun.

--- fastpbkdf2.c
+++ fastpbkdf2.c
@@ -16,6 +16,9 @@
 
 #include <assert.h>
 #include <string.h>
+#if defined(__GNUC__)
+#include <endian.h>
+#endif
 
 #include <openssl/sha.h>
 
@@ -30,7 +33,7 @@
 
 static inline void write32_be(uint32_t n, uint8_t out[4])
 {
-#if defined(__GNUC__) && __GNUC__ >= 4 && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#if defined(__GNUC__) && __GNUC__ >= 4 && __BYTE_ORDER == __LITTLE_ENDIAN
   *(uint32_t *)(out) = __builtin_bswap32(n);
 #else
   out[0] = (n >> 24) & 0xff;
@@ -42,7 +45,7 @@ static inline void write32_be(uint32_t n, uint8_t out[4])
 
 static inline void write64_be(uint64_t n, uint8_t out[8])
 {
-#if defined(__GNUC__) &&  __GNUC__ >= 4 && __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+#if defined(__GNUC__) &&  __GNUC__ >= 4 && __BYTE_ORDER == __LITTLE_ENDIAN
   *(uint64_t *)(out) = __builtin_bswap64(n);
 #else
   write32_be((n >> 32) & 0xffffffff, out);
